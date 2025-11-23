956414e864e26d7462d2aa0fe481c5d9062a011f
nikita_staricyn <nikistar986@gmail.com>
2025-11-24 02:37:29 +0300

    Поднял compileSdk и targetSdk до 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.xando.staya"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.xando.staya"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
