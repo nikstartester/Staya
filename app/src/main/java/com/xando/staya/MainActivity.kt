8f17b7b5e9965f91869684f566af6835f59574f3
nikita_staricyn <nikistar986@gmail.com>
2025-11-24 23:28:48 +0300

    Простая реализация навигации в модуле app: - RootHost() подключает главный NavHost, который содержит граф логина и главный экран с нижней навигацией (navigationBarHost()) - navigationBarHost() содержит bottomBar со своим NavHost и логикой закрытия приложения при нажатии на кнопку назад (AppCloseBackHandler())

--- app/src/main/java/com/xando/staya/MainActivity.kt
+++ app/src/main/java/com/xando/staya/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.xando.staya.ui.RootHost
 import com.xando.staya.ui.theme.StayaTheme
 
 /**@SelfDocumented*/
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             StayaTheme {
-
+                RootHost()
             }
         }
     }
