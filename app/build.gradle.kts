3c0c4fe59e9412002bcd8088fe3b2a9251de3227
nikita_staricyn <nikistar986@gmail.com>
2025-11-24 03:53:40 +0300

    Вынес базовые настройки модулей в android-app-plugin для app модуля и android-base-plugin для остальных модулей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,43 +1,25 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("android-app-plugin")
     alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.xando.staya"
-    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.xando.staya"
-        minSdk = 26
-        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
---
+++ gradle-settings/convention/src/main/kotlin/android-app-plugin.gradle.kts
@@ -0,0 +1,41 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+extensions.configure<ApplicationExtension> {
+    compileSdk = libs.findVersion("projectCompileSdk").get().requiredVersion.toInt()
+
+    defaultConfig {
+        minSdk = libs.findVersion("projectMinSdk").get().requiredVersion.toInt()
+        targetSdk = libs.findVersion("projectTargetSdk").get().requiredVersion.toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
+}
