864092906fbfec4ed4e50e829d87217838c3edc8
nikita_staricyn <nikistar986@gmail.com>
2025-12-21 02:37:32 +0300

    Начало работ по темизации

--- app/src/main/java/com/xando/staya/ui/theme/Theme.kt
+++ app/src/main/java/com/xando/staya/ui/theme/Theme.kt
@@ -1,26 +1,26 @@
 package com.xando.staya.ui.theme
 
-import android.app.Activity
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+import com.xando.design.ui.theme.ExtendedColors
+import com.xando.design.ui.theme.LocalExtendedColors
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    primary = Color(0xFF466677),
     secondary = PurpleGrey80,
     tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Color(0xFF466677),
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    background = Color(0xFFFFFFFF),
+    surface = Color(0xFFEFF5FC)
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,25 +33,37 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val LightExtendedColors = ExtendedColors(
+    textColor = Color(0xFF000000),
+
+    iconColor = Color(0xFF333333),
+
+    primaryTextColor = LightColorScheme.primary,
+    primaryIconColor = LightColorScheme.primary,
+
+    contrastTextColor = Color(0xFFFFFFFF),
+    contrastIconColor = Color(0xFFFFFFFF),
+
+    unaccentedTextColor = Color(0xFF797979),
+    unaccentedIconColor = Color(0xFF797979)
+)
+
+// Пока без темной темы
+private val DarkExtendedColors = LightExtendedColors
+
+
 @Composable
 fun StayaTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    darkTheme: Boolean = false, //isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
+    val extendedColors = if (darkTheme) DarkExtendedColors else LightExtendedColors
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    CompositionLocalProvider(LocalExtendedColors provides extendedColors) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = Typography,
+            content = content
+        )
     }
-
-    MaterialTheme(
-      colorScheme = colorScheme,
-      typography = Typography,
-      content = content
-    )
 }
