8c58e79346fb7bc1dfcfd3d39c0ba736b76e12af
nikita_staricyn <nikistar986@gmail.com>
2025-12-23 00:04:18 +0300

    Добавил стандартную анимацю для переходов экранов

---
+++ core/design/src/main/java/com/xando/design/animations/TransitionAnimations.kt
@@ -0,0 +1,41 @@
+package com.xando.design.animations
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+
+private const val ANIM_DURATION = 300
+private const val OFFSET_PERCENT = 80
+
+/**
+ * Content transform без какой-либо анимации
+ */
+val noTransition: ContentTransform = EnterTransition.None togetherWith ExitTransition.None
+
+/**
+ * Анимация появления нового контента справа с лёгким fade-in.
+ * Используется как "forward" переход.
+ */
+fun AnimatedContentTransitionScope<*>.rightInTransition(): ContentTransform =
+    slideInHorizontally(
+        initialOffsetX = { fullWidth -> fullWidth / 100 * OFFSET_PERCENT },
+        animationSpec = tween(ANIM_DURATION)
+    ) + fadeIn(tween(ANIM_DURATION)) togetherWith ExitTransition.None
+
+/**
+ * Анимация ухода текущего контента вправо с лёгким fade-out.
+ * Используется как "backward" переход.
+ */
+fun AnimatedContentTransitionScope<*>.rightOutTransition(): ContentTransform =
+    EnterTransition.None togetherWith slideOutHorizontally(
+        targetOffsetX = { fullWidth -> fullWidth / 100 * OFFSET_PERCENT },
+        animationSpec = tween(ANIM_DURATION)
+    ) + fadeOut(tween(ANIM_DURATION))
+
