d3393a92e906bfa568b5fdc998ef9df99f133854
nikita_staricyn <nikistar986@gmail.com>
2026-03-24 23:24:18 +0300

    Небольшие правки

--- data/auth/src/main/java/com/xando/data/auth/model/ResendCodeRequest.kt
+++ data/auth/src/main/java/com/xando/data/auth/model/ResendCodeRequest.kt
@@ -5,7 +5,7 @@ package com.xando.data.auth.model
 import kotlinx.serialization.InternalSerializationApi
 import kotlinx.serialization.Serializable
 
-/** Модель запроса переотправки кода подтверждения. */
+/** Модель запроса повторной отправки кода подтверждения. */
 @Serializable
 internal data class ResendCodeRequest(
     val email: String,
