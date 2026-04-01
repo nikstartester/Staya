9aeadb062f5f2742371a35e341b7d4c9acaa4f17
nikita_staricyn <nikistar986@gmail.com>
2026-04-02 02:57:22 +0300

    Пока не поддерживаем темную тему иконки статусов будут темные

--- app/src/main/java/com/xando/staya/MainActivity.kt
+++ app/src/main/java/com/xando/staya/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.xando.staya
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.xando.navigation_api.EntryBuilder
@@ -23,7 +25,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            // Пока не поддерживаем темную тему
+            statusBarStyle = SystemBarStyle.light(scrim = Color.TRANSPARENT, darkScrim = Color.TRANSPARENT)
+        )
         setContent {
             StayaTheme {
                 RootHost(entryBuilders)
