a9ce10a2b3ce89590b45874a3fa8b56e2f0d957a
nikita_staricyn <nikistar986@gmail.com>
2026-04-02 22:18:15 +0300

    Добавил пример как подключиться к серверу с реального устройства

--- core/network/src/main/java/com/xando/core/network/stand/Stand.kt
+++ core/network/src/main/java/com/xando/core/network/stand/Stand.kt
@@ -6,6 +6,10 @@ package com.xando.core.network.stand
  * @property baseUrl Базовый URL стенда, к которому будут направляться запросы.
  */
 enum class Stand(val baseUrl: String) {
+    // Для устройства (ip адрес компа с сервером в локальной сети)
+    /*PROD("http://192.168.1.120:8080/api/v1"),
+    TEST("http://192.168.1.120:8080/api/v1")*/
+    // Для эмулятора
     PROD("http://10.0.2.2:8080/api/v1"),
     TEST("http://10.0.2.2:8080/api/v1")
 }
