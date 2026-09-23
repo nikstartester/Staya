95c44fecd93b516d27df550837a4df6c40f9756e
nikita_staricyn <nikistar986@gmail.com>
2026-09-23 07:00:21 +0300

    Подключение Room 3 в core:database

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     alias(libs.plugins.staya.android.baseCfg)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.room3)
 }
 
 android {
     namespace = "com.xando.core.database"
 }
 
-dependencies {
+room3 {
+    schemaDirectory("$projectDir/schemas")
+}
 
+dependencies {
+    // Room
+    api(libs.androidx.room3.runtime)
+    implementation(libs.androidx.sqlite.framework)
+    ksp(libs.androidx.room3.compiler)
 }
